e590112e734f5a11bc4af21a018c78fef2d2ade6
Lucas Paulger <lucas.paulger@gmail.com>
2011-12-22 01:32:27 -0500

    fix post to white page

--- application/controllers/tasks.php
+++ application/controllers/tasks.php
@@ -97,64 +97,29 @@ class Tasks extends CI_Controller {
 		else
 		{
 			$data['update'] = $this->TaskModel->updateUser();
+			redirect('/','location');
 		}
 	}
 
 	function create($username = null)
 	{
-		if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH']=="XMLHttpRequest")
-		{
-			$this->load->model('TaskModel');
-			$this->load->model('UserModel');
-			$this->load->library('form_validation');
-			$this->load->helper('form');
-			$this->lang->load('task');
-			
-			$this->form_validation->set_rules('name', 'name', 'required');
-			$session = $this->session->all_userdata();
-			if(isset($session['logged_in']) && $session['logged_in']==TRUE){
-				
-				if ($this->form_validation->run() == FALSE)
-				{
-					$this->load->view('tasks/create');
-				}
-				else
-				{
-					$this->TaskModel->addTask();
-					redirect('/','location');
-				}
-			}
-        }
-        else
-        {
-			$this->load->model('TaskModel');
-			$this->load->model('UserModel');
-			$this->load->helper('form');
-			$this->load->library('form_validation');
-			$this->lang->load('task');
-			
-			$this->form_validation->set_rules('taskName', 'task name', 'required');
-			
-			$this->load->view('default/header');
-			$this->load->view('authentication/loginForm');
-			
-			$session = $this->session->all_userdata();
-			if(isset($session['logged_in']) && $session['logged_in']==TRUE){
-				
-				if ($this->form_validation->run() == FALSE)
-				{
-
-				}
-				else
-				{
-					$this->TaskModel->addTask();
-					redirect('/','location');
-				}
-				$this->load->view('default/nav');
-				$this->load->view('tasks/create');
-			}
+		$this->load->model('TaskModel');
+		$this->load->model('UserModel');
+		$this->load->library('form_validation');
+		$this->load->helper('form');
+		$this->lang->load('task');
 		
-		$this->load->view('default/footer');
-       }
+		$this->form_validation->set_rules('taskName', 'taskName', 'required');
+		$session = $this->session->all_userdata();
+			
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('tasks/create');
+		}
+		else
+		{
+			$this->TaskModel->addTask();
+			redirect('/','location');
+		}
 	}
 }
