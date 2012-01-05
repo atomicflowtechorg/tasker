f0e7aa3e6e001e6ab8cb593478612de663313585
Lucas Paulger <lucas.paulger@gmail.com>
2012-01-05 01:43:03 -0500

    fix search help box location

--- js/taskscript.js
+++ js/taskscript.js
@@ -1,5 +1,5 @@
 
-function loadLink(link, area){
+function loadLink(link, area) {
     $("#" + area).fadeOut(500, function(){
         $.ajax({
             url: $(link).attr("href"),
@@ -15,9 +15,6 @@ $(document).ready(function(){
     $("#appnav li").blend();
     $('#appnav').hide();
     $('#appnav').fadeIn("fast");
-	
-	
-	
     //******* ajax load link data *******************************************************
     $(".ajax_anchor_load").live("click", function(){
         loadLink(this, "content");
@@ -49,40 +46,37 @@ $(document).ready(function(){
         itemEasing		: 'easeOutExpo',
         // number of items to scroll at a time
         scroll			: 1
-    });
-        
+    });   
     $("#selectTeamOption").live("change", function(){
         var teamName = $(this).val();
         teamName = teamName.replace(/\s/g,"-");
         var $ca = $(".ca-container");
         var link = "teams/show/" + teamName;
-        $ca.fadeOut(function(){
+        $ca.fadeOut(function () {
             $.ajax({
                 url: link,
                 cache: false,
-                success: function(html){
+                success: function (html) {
                     $ca.html(html);
                     $ca.contentcarousel();
                     $ca.fadeIn();
                 }
-            }); 
-        });   
+            });
+        });
         return false;
     });
-    
-    $('#searchForm').submit(function() {
-        $("#content").fadeOut(500, function(){
-            var search = "searchBoxInput="+$("#searchBoxInput").val();
+    $('#searchForm').submit(function () {
+        $("#content").fadeOut(500, function () {
+            var search = "searchBoxInput=" + $("#searchBoxInput").val();
             $.ajax({
                 type: 'POST',
                 url: $('#searchForm').attr("action"),
                 data: search,
-                success: function(html){
+                success: function (html) {
                     $("#content").fadeIn(500).html(html);
                 }
             });
         });
         return false;
     });
-	
-})//end ready function
+}); // end ready function
