1d295b896f1cce247a0aca53bbd0375f0b09853d
Lucas Paulger <lucas.paulger@gmail.com>
2012-01-09 19:34:21 -0500

    Made new user changes so a member with no team can't access tasker.

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,51 +1,81 @@
 <?php
+
 /**
-* @property CI_Loader $load
-* @property CI_Form_validation $form_validation
-* @property CI_Input $input
-* @property CI_Email $email
-* @property CI_DB_active_record $db
-*/
+ * @property CI_Loader $load
+ * @property CI_Form_validation $form_validation
+ * @property CI_Input $input
+ * @property CI_Email $email
+ * @property CI_DB_active_record $db
+ */
 class Dashboard extends CI_Controller {
 
-	public function index()
-	{   
-                $this->load->model('teamModel');
-                $this->load->model('TaskModel');
-		$this->load->helper('form');
-                
-		$data['session'] = $session = $this->session->all_userdata();
-                
-                //load current users tasks by default
-                $data['user'] = $session['username'];
-		$data['title'] = lang('individual_title',array('My'));
-                $data['taskList'] = $this->TaskModel->getTasksForTasker($data['user']);
-		$data['listUrl'] = site_url("lists/showUserLists/".$data['user']);
-		$data['empty_list'] = lang('error_user_noTasks',array($data['user'],$data['user']));
-                
-                
-                $teams = $this->teamModel->getTeamsForUser($session['username']);               
-                $team = $teams[0]->fkTeamName;
-                $data['teams'] = $teams;
-                $data['users'] = $this->teamModel->getUsersForTeam(true , $team);
-                foreach($data['users'] as $user){
-                    $email = "someone@somewhere.com";
-                    $default = base_url()."images/profiles/default.jpg";
-                    $size = 100;
-                    $grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $user->fldEmail ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
-                    $user->fldProfileImage = $grav_url;
-                }
-                $this->lang->load('authentication');
-                $this->lang->load('team');
+    public function index() {
+        $this->load->model('teamModel');
+        $this->load->model('TaskModel');
+        $this->load->model('userModel');
+        $this->load->helper('form');
 
-		$this->load->library('form_validation');
-		$this->load->view('default/head');
-		$this->load->view('default/toolbar',$data);
-                
-                $this->load->view('teams/viewTeam', $data);
-                
-                $this->load->view('default/searchBox');
-		$this->load->view('taskListMasterView',$data);
-		$this->load->view('default/footer');
-	}
-}		
+        $data['session'] = $session = $this->session->all_userdata();
+
+        //load current users tasks by default
+        $data['user'] = $session['username'];
+        $data['title'] = lang('individual_title', array('My'));
+        $data['taskList'] = $this->TaskModel->getTasksForTasker($data['user']);
+        $data['listUrl'] = site_url("lists/showUserLists/" . $data['user']);
+        $data['empty_list'] = lang('error_user_noTasks', array($data['user'], $data['user']));
+        
+        $teams = $this->teamModel->getTeamsForUser($session['username']);
+        
+        $hasTeam = false;
+        if (count($teams) == 0) {
+            
+        } else {
+            $hasTeam = true;
+            $team = $teams[0]->fkTeamName;
+            $data['users'] = $this->teamModel->getUsersForTeam(true, $team);
+
+            //Set current user as first 
+            $users = $data['users'];
+            $pos = 0;
+            foreach ($users as $user) {
+                if ($user->pkUsername == $session['username']) {
+                    $currentUser = $users[$pos];
+                    unset($users[$pos]);
+                    array_unshift($users, $currentUser);
+                }
+                $pos++;
+            }
+            $data['users'] = $users;
+            //END Set current user as first
+
+            $data['teams'] = $teams;
+
+            foreach ($data['users'] as $user) {
+                $default = base_url() . "images/profiles/default.jpg";
+                $size = 100;
+                $grav_url = "http://www.gravatar.com/avatar/" . md5(strtolower(trim($user->fldEmail))) . "?d=" . urlencode($default) . "&s=" . $size;
+                $user->fldProfileImage = $grav_url;
+            }
+        }// end have team check
+
+
+        $this->lang->load('authentication');
+        $this->lang->load('team');
+
+        $this->load->library('form_validation');
+        $this->load->view('default/head');
+        $this->load->view('default/toolbar', $data);
+        
+        if($hasTeam){
+            $this->load->view('teams/viewTeam', $data);
+            $this->load->view('default/searchBox');
+            $this->load->view('taskListMasterView', $data);
+        }else{
+            $this->load->view('teams/none',$data);
+        }
+        
+        $this->load->view('default/footer');
+    }
+
+}
+
--- application/hooks/template.php
+++ application/hooks/template.php
@@ -1,21 +1,24 @@
 <?php
-class Template{
-	var $CI;
-	function Template() {
-        $this->CI =& get_instance();
-    }
-	
-	function authenticationCheck() {
 
-		$session = $this->CI->session->all_userdata();
-		if(isset($session['logged_in']) && $session['logged_in']==TRUE){
-			
-		}
-		else{
-			$controller = $this->CI->uri->segment(1);
-			if($controller !== "authentication"){
-				redirect('authentication', 'location');
-			}
-		}
-	}
+class Template {
+
+    var $CI;
+
+    function Template() {
+        $this->CI = & get_instance();
+    }
+
+    function authenticationCheck() {
+        $this->CI->load->model('teamModel');
+        $session = $this->CI->session->all_userdata();
+        
+         if (isset($session['logged_in']) && $session['logged_in'] == TRUE) {
+            
+        } else {
+            $controller = $this->CI->uri->segment(1);
+            if ($controller !== "authentication") {
+                redirect('authentication', 'location');
+            }
+        }
+    }
 }
