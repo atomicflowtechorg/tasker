90c1e010080f413811165243cbafa8bf67151da9
Brandon John-Freso <brandonjf@atomicflowtech.com>
2012-01-21 21:59:43 -0500

    Fixing config'd port to resolve connection issue.

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ if (!defined('BASEPATH'))
   | path to your installation.
   |
  */
-$config['base_url'] = 'http://localhost:8080/tasker/';
+$config['base_url'] = 'http://localhost/tasker/';
 
 /*
   |--------------------------------------------------------------------------
