93a6ba3fd03cc1c085226f11da5aac4dd2aeb0e2
Lucas Paulger <lucas.paulger@gmail.com>
2012-01-26 19:54:25 -0500

    WIP on loading task list

--- js/listView.js
+++ js/listView.js
@@ -1,7 +1,37 @@
 /* Author: Lucas Paulger*/
+var Task = Spine.Model.sub();
+Task.configure("Task", "id","title","status");
 
-function list(){
-    this.MaxLength = 5;
-    this.items = new Array();
+Task.extend({
+
+});
+
+Task.include({
+
+    });
+
+
+var List = {
+    tasks: new Array(),
+    add: function(taskItem){
+        this.tasks.push(taskItem)
+    }
 }
+
+$.ajax({
+    url: "http://localhost/tasker/index.php/tasks/load/"+username,
+    cache: false,
+    success: function(data){
+        for (task in data){
+            task = new Task({id:task.pkTaskId,title:task.fldName,status:task.fldStatus})
+            List.add(task);
+        }
+    }
+});
+
+for(task in List.tasks){
+    var info = "ID: " + task.id + "title: " + task.title;
+}   
+
+
 
