0ed680b901a4a0117c3b1e35a92ce363b0939b9f
U-Prometheus\Brandon <Brandon@Prometheus.(none)>
2012-01-29 16:41:29 -0500

    Login changes

--- application/views/default/header.php
+++ application/views/default/header.php
@@ -2,6 +2,5 @@
 <header id="topHeader">
 		<a href="<?php echo base_url(); ?>"><h1>TeamTasker V1</h1></a>
 		<h2>Welcome. Please Sign-In.</h2>
-		<div class="circle"></div>
-<div class="circle1"></div>
+		
 </header>
